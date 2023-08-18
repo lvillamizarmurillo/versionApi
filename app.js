4b0c398094faad33ecff39923e74fb077cea33ed
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-08-18 16:57:55 -0500

    feat: :sparkles: Versiones ya funcionales
    Ya se aplico las versiones funcionales, solo queda programar los endpoints

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import routesVersioning from "express-routes-versioning";
 dotenv.config()
+let prueba = routesVersioning();
 let app = express();
 
 app.use(express.json());
+app.use(function(req,res,next){
+    req.version = req.headers['accept-version'];
+    next();
+})
+app.get('/test', prueba({
+    "1.0.0": ver1,
+    "1.0.1": ver2
+}))
+
+function ver1(req,res){
+    res.status(200).send({status: 200, message: "Estas usando la version 1.0.0"})
+}
+function ver2(req,res){
+    res.status(200).send({status: 200, message: "Estas usando la version 1.0.1"})
+}
+
 const config = JSON.parse(process.env.MY_SERVER)
 app.listen(config, ()=>{
     console.log(`http://${config.hostname}:${config.port}`);
---
+++ db/atlas.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { MongoClient } from 'mongodb';
+dotenv.config("../");
+export async function connect(){
+    try {
+        const uri = `mongodb+srv://${process.env.MY_NAME}:${process.env.MY_PASSWORD}@cluster0.1wthqi6.mongodb.net/${process.env.MY_DB}`
+        const option = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        const client = await MongoClient(uri, option);
+        return client.db();
+    } catch (error) {
+        return {status: 500, message: error};
+    }
+}
