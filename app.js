fc65e9f54c79da0bccc7a19ab0ec3f5978b11a84
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-08-18 16:02:18 -0500

    feat: :tada: PracticaVersionApi
    Se creara un programa basico con endpoints sin capas de seguridad, unicamente para practicar el VersionApi

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+let app = express();
+
+app.use(express.json());
+const config = JSON.parse(process.env.MY_SERVER)
+app.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}`);
+})
