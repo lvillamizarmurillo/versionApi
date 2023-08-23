85e7c4e494fb1bbb239e56e5d29ae12c36d58818
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-08-23 15:53:49 -0500

    feat: :sparkles: Creando JWT
    Se crea con el proposito de versionarlo con las apis y dependiendo del rol y de los permisos que tenga pueda acceder

--- config/JWT.js
+++ config/JWT.js
@@ -6,5 +6,40 @@ dotenv.config("../");
 
 const conexionDB = connection();
 const createToken = async(req,res,next)=>{
-    
+    if(Object.keys(req.body).length === 0) return res.status(400).send({status: 400, message: "Datos no enviados"});
+    const result = await conexionDB.collection("autor").findOne(req.body);
+    console.log(result);
+    if(!result) return res.status(401).send({status: 400, message: "Autor no encontrado"});
+    const encoder = new TextEncoder();
+    const id = result._id.toString();
+    const jwtConstructor = await new SignJWT({id: id})
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT'})
+        .setIssuer()
+        .setExpirationTime('3h')
+        .sign(encoder.encode(process.env.JWT));
+    req.data = {status:200, message: jwtConstructor};
+    next();
+}
+const validarToken = async(req,token)=>{
+    try {
+        encoder = new TextEncoder();
+        const jwtData = await jwtVerify(
+            token,
+            encoder.encode(process.env.JWT)  
+        );
+        let res = await conexionDB.collection("autor").findOne(
+            {
+                _id: new ObjectId(jwtData.payload.id),
+                [`permisos.${req.baseUrl}`]: `${req.headers["accept-version"]}`
+            }
+        );
+        let {id,permisos, ...autor} = res;
+        return autor;
+    } catch (error) {
+        return false;
+    }
+}
+export {
+    createToken,
+    validarToken
 }
